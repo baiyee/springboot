9bbe18ed69a5a497f3687671714480c693f6fe71
zhangxuan <707161293@qq.com>
2019-04-30 16:12:55 +0800

    add java lambda 表达式 demo

---
+++ src/main/java/com/example/springboot/lambda/Base.java
@@ -0,0 +1,9 @@
+package com.example.springboot.lambda;
+
+public class Base {
+
+    public String toUpper(String str) {
+        System.out.println("base to upper");
+        return str.toUpperCase();
+    }
+}
---
+++ src/main/java/com/example/springboot/lambda/Example.java
@@ -0,0 +1,15 @@
+package com.example.springboot.lambda;
+
+import java.util.function.Function;
+
+public class Example {
+    public static void main(String[] args) {
+        /**
+         * 输入一个字符串，返回输入字符串的大写
+         * aaa -> AAA
+         */
+        Function<String,String> fn = (str) -> str.toUpperCase();
+        System.out.println(fn.apply("admin"));
+        System.out.println("test coding");
+    }
+}
