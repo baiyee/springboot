7e9877181ed8b37d4024d43fba72eedee9032798
张宇璇 <zhangxuan@do1.com.cn>
2021-07-14 14:07:46 +0800

    算法：删除排序数组中的重复项  添加解题思路 审查人：self

--- src/main/java/com/zx/springboot/licode/list/RemoveDuplicates.java
+++ src/main/java/com/zx/springboot/licode/list/RemoveDuplicates.java
@@ -1,6 +1,5 @@
 package com.zx.springboot.licode.list;
 
-import org.apache.poi.hssf.record.DVALRecord;
 
 import java.util.Arrays;
 
@@ -9,18 +8,48 @@ import java.util.Arrays;
  * @description: 删除排序数组中的重复项
  * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
  * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ * <p>
+ * 解题思路：有序数组，那么数组中的元素，相同必定相连。
+ * 使用双指针解决，
+ * 1、初始值：left指针= 0，right指针 = 1
+ * 2、遍历数组，使用right 指针， 当left指针的值与right指针的值不同时，
+ * 将left指针向后移动一位，并将right指针当前值赋值给left指针移动后的值。
+ * 遍历完整个数组，返回left指针下标。
  * @date 2021/7/14
  */
 public class RemoveDuplicates {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int i = s.removeDuplicates(new int[]{1, 1, 2});
+        int a = 0;
+        int i = a++;
         System.out.println(i);
-        int i1 = s.removeDuplicates1(new int[]{1, 1, 2, 2, 2});
-        System.out.println(i1);
+        System.out.println(a);
+        // int i = s.removeDuplicates(new int[]{1, 1, 2});
+        // System.out.println(i);
+        // int i1 = s.removeDuplicates1(new int[]{1, 1, 2, 2, 2});
+        // System.out.println(i1);
+        int result = s.removeDuplicatesByZx(new int[]{1, 1, 2, 2});
+        System.out.println(result);
     }
 
+
+
     static class Solution {
+        int removeDuplicatesByZx(int[] a) {
+            //判空
+            if (a == null || a.length == 0) {
+                return 0;
+            }
+            int left = 0;
+            for (int right = 1; right < a.length; right++) {
+                if (a[right] != a[left] ) {
+                    a[++left] = a[right];
+                }
+            }
+            System.out.println(left);
+            return ++left;
+        }
+
         //双指针解决
         int removeDuplicates(int[] A) {
             //边界条件判断
@@ -39,20 +68,21 @@ public class RemoveDuplicates {
             System.out.println(Arrays.toString(A));
             return ++left;
         }
+
         //双指针，数组长度减去重复数字个数解决
         int removeDuplicates1(int[] A) {
             int count = 0;// 重复的数字个数
-            for (int right = 1; right < A.length; right ++) {
+            for (int right = 1; right < A.length; right++) {
                 if (A[right] == A[right - 1]) {
                     //如果有重复，count 值加1
-                    count ++;
+                    count++;
                 } else {
                     //如果没有重复，后面的就往前挪
                     A[right - count] = A[right];
                 }
             }
             //数组长度减去重复的个数
-            return A.length - count ;
+            return A.length - count;
         }
 
         int removeDuplicates2(int[] nums, int numsSize) {
