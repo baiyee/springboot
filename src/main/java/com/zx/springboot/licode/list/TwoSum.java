8264a9f17a3f98fda5af74a3357cd85e8ae9a6c0
张宇璇 <zhangxuan@do1.com.cn>
2021-07-15 10:21:29 +0800

    算法：求两数之和  添加解题思路 审查人：self

---
+++ src/main/java/com/zx/springboot/licode/list/TwoSum.java
@@ -0,0 +1,29 @@
+package com.zx.springboot.licode.list;
+
+/**
+ * @description: TODO 求两数之和，使用暴力双重遍历  
+ * @date 2021/7/14
+ * @author zhangxuan
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+        System.out.println(Solution.twoSum(new int[]{3,2,4},6));
+    }
+   static class Solution {
+        public static int[] twoSum(int[] nums, int target) {
+            if (nums == null || nums.length == 0) {
+                return new int[]{};
+            }
+            for (int i = 0; i < nums.length; i ++) {
+                for (int j = i+1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] != target) {
+                        continue;
+                    } else {
+                        return new int[]{i,j};
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
