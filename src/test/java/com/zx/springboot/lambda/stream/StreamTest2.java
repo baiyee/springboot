f5f484aae2ecbfa6a7818566f95e8493aa565721
张宇璇 <zhangxuan@do1.com.cn>
2021-07-16 15:55:15 +0800

    stream lambda 表达式  Stream流实战2  flatMap  连接多个 list 或 map 对象 审查人：self

--- src/test/java/com/zx/springboot/lambda/stream/StreamTest2.java
+++ src/test/java/com/zx/springboot/lambda/stream/StreamTest2.java
@@ -1,9 +1,9 @@
 package com.zx.springboot.lambda.stream;
 
+import com.google.common.collect.Maps;
 import net.sf.ezmorph.test.ArrayAssertions;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -24,5 +24,40 @@ public class StreamTest2 {
         // 所以list.toString 方法其实就是调用的abstractCollection抽象类的toString方法.
         System.out.println(together.toString());
         ArrayAssertions.assertEquals(Arrays.asList(1,2,3,4),together);
+        testArrays();
+    }
+
+    /**
+     * 数组连接有两种方法
+     * 方法一： list.addAll() 传入一个数组 注意：参数只能传一个 不支持多个参数
+     * 方法二：Stream.flatMap() 传入多个对象，支持list、map、set、object等
+     */
+    public static void testArrays() {
+        //stream flatMap 连接多个list 对象
+        List<String> list1 = new ArrayList<>();
+        list1.add("a");
+        list1.add("b");
+        list1.add("c");
+        System.out.println(list1.toString());
+        List<String> list2 = new ArrayList<>();
+        list2.addAll(list1);
+        list2.add("b");
+        list2.add("c");
+        list2.add("d");
+        System.out.println(list2.toString());
+        List<String> results = Stream.of(list1, list2).flatMap(strings -> strings.stream())
+                .collect(Collectors.toList());
+        System.out.println(results);
+
+        //stream flatMap map 对象
+        Map<String,String> map1 = new HashMap<>();
+        map1.put("z","x");
+        map1.put("h","z");
+        Map<String,String> map2 = new HashMap<>();
+        map1.put("z2","x2");
+        map1.put("h2","z2");
+        List<String> results2 = Stream.of(map1, map2).flatMap(strings -> strings.values().stream())
+                .collect(Collectors.toList());
+        System.out.println(results2);
     }
 }
